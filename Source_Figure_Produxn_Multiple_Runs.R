c9e433a8f31d52eeaaac988a2699d9b0391a2c17
Parker Rundstrom <parker.rundstrom@gmail.com>
2018-10-30 12:54:20 -0500

    progress...

--- Source_Figure_Produxn_Multiple_Runs.R
+++ Source_Figure_Produxn_Multiple_Runs.R
@@ -2,9 +2,9 @@
 parent_directory <- getwd()
 for(run_visual in 1:number_of_runs) {
   if(run_visual == 1) {
-    FolderName <- format(Sys.time(), "%F-%H%M%S")
-    dir.create(file.path(parent_directory, paste0(FolderName, "-GMT-multirun-output")))
-    FolderName <- paste0(parent_directory, "/", FolderName, "-GMT-multirun-output")
+    multiRunTime <- format(Sys.time(), "%F-%H%M%S")
+    dir.create(file.path(parent_directory, paste0(multiRunTime, "-GMT-multirun-output")))
+    FolderName <- paste0(parent_directory, "/", multiRunTime, "-GMT-multirun-output")
     saveRDS(object = multiRun_folderList, file = paste0(FolderName, "/folderList.RData"))
     
   } # makes the folder for multirun results, saves the multiRun_folderList there.
@@ -17,6 +17,7 @@ for(run_visual in 1:number_of_runs) {
   setwd(strsplit(run_number_directory, "20")[[1]][1])
   multiRun_folderList <- readRDS(file = paste0(getwd(), "/", tail(list.files(pattern = "multirun"),1), "/folderList.RData"))
   parent_directory <- getwd()
+  multirun_directory <- paste0(parent_directory, tail(list.files(pattern = "multirun"),1))
   #results_directory <- paste0(str_split(parent_directory, "Curiosity")[[1]][1], "Results/")
   setwd(run_number_directory)
   
@@ -27,11 +28,13 @@ for(run_visual in 1:number_of_runs) {
   
   R <- create_plot_info(info[[2]], info[[1]])
   
-  stuff <- paste(paste0("sink(file = \"", multiple_files, " - Parameters and Info\")"), 
+  stuff <- paste(paste0("sink(file = \"", run_visual, " - Parameters and Info\")"), 
                  "cat(paste0(\"Number of Timesteps: \", info[[3]][1], \"\nNumber of Populations: \", info[[3]][2], \"\nPopulation Size: \", info[[3]][3], \"\nNumber of Syllables: \", info[[3]][4], \"\nNumber of Syllable Positions Assigned to Specific Probability Levels: \", info[[3]][5], \"\nNumber of Singers Sampled from One Population for Mating: \", info[[3]][7], \"\nNumber of Singers Sampled from One Population for Tutoring: \", info[[3]][6], \"Probability of Inheriting Curiosity Accurately: \", info[[3]][8], \"\nProbability of Learning Syllables Accurately from Parent: \", info[[3]][10], \"\nProbability of Learning Syllables Accurately from Tutor: \", info[[3]][9], \"\nProbability of Picking up Random Extra Syllables from Parent: \", info[[3]][12], \"\nProbability of Picking up Random Extra Syllables from Tutor: \", info[[3]][11], \"\nStandard Deviation of Randomly-picked-up Sylls from Established Mean: \", info[[3]][13], \"\nNumber of Rows in Population Calls Matrix: \", info[[3]][14], \"\nNumber of Columns in Pop Calls Matrix: \", info[[3]][15], \"\nPairing Pool Rows: \", info[[3]][16], \"\nPairing Pool Columns: \", info[[3]][17], \"\nPairing Pool Slices: \", info[[3]][18], \"\nCuriosity Counter Rows: \", info[[3]][19], \"\nCuriosity Counter Columns: \", info[[3]][20], \"\nPopulation Syllable Probability Rows: \", info[[3]][21], \"\nPopulation Probability Columns: \", info[[3]][22], \"\nLength of Curiosity Breaks Vector: \", info[[3]][23], \"\nLength of Zero to One Template: \", info[[3]][24], \"\nLearning Pool Rows: \", info[[3]][25], \"\nLearning Pool Columns: \", info[[3]][26], \"\nLearning Pool Slices: \", info[[3]][27]))", 
                  "sink()", sep = "\n")
   eval(parse(text=stuff))
   
+  
+}
 
   
   sylrepblahz <- paste0("sylrepz", run_visual, " <- split_data(converted_data, 1)")
