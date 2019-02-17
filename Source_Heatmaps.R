24a02a9dd02c2d7887432175f4d7a96b72aa1f0d
Parker Rundstrom <parker.rundstrom@gmail.com>
2019-02-17 08:40:24 -0600

    some progress

--- Source_Heatmaps.R
+++ Source_Heatmaps.R
@@ -1,12 +1,18 @@
-setwd("~/Documents/projects/Code/Results/")
+#setwd("~/Documents/projects/Code/Results/")
+setwd("~/Downloads/Lab_Notebook/GitHub/Code/Results")
 if(!(dir.exists("Heatmaps"))) {dir.create("Heatmaps")}
 setwd("Heatmaps")
 heatmapLand <- getwd()
-setwd("../../../../../../../../../../media/parker/A443-E926/simulation runs/shifting_curstart_for_heatmap/")
+#setwd("../../../../../../../../../../media/parker/A443-E926/simulation runs/shifting_curstart_for_heatmap/")
+setwd("/Volumes/NO NAME/simulation runs/shifting_curstart_for_heatmap/")
 heatmap_runs <- list.files()
 diffMale_start <- heatmap_runs[grep("[0-9]mp[0-9]", heatmap_runs)]
+  # 
+  diffMale_start[c(1,2,3,4,5,6,7,31,46,9,10,11,12,47,13,14,15,48,16,17,18,19,49,20,21,22,23,24,25,26,27,28,29,32,33,34,35,36,37,30,50,38,39,40,41,42,)]
 sameMale_start <- heatmap_runs[grep("[0-9]m_", heatmap_runs)]
+  # sameMale_start <- sameMale_start[18,1,2,16,3,5,4,6,14,19,15,20,10,11,13,12,7,9,17,8]
 allSame_start <- heatmap_runs[grep("[0-9]f", heatmap_runs, invert = T)]
+  # allSame_start <- allSame_start[2,4,3,1,5]
 
 ending_curiosity_mp1 <- array(0, c(5,5,5), list(c("1-7mp1", "7-13mp1", "11-26mp1", "1-26mp1", "11-15mp1"), c("1-7mp2", "7-13mp2", "11-26mp2", "1-26mp2", "11-15mp2"), c("1-7f", "7-13f", "11-26f", "1-26f", "11-15f")))
   # sumStats[8]
