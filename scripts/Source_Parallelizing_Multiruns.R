3fae6da2b5b25da758ed3b29acddeab384108e66
Parker Rundstrom <parker.rundstrom@gmail.com>
2019-03-02 17:46:49 -0600

    parallel for laptop

--- scripts/Source_Parallelizing_Multiruns.R
+++ scripts/Source_Parallelizing_Multiruns.R
@@ -2,14 +2,14 @@ rm(list=objects())
 source("Source_Reference_Section.R")
 
 # we specify the number of cores/workers we want to use
-n_cores <- detectCores() - 4
+n_cores <- detectCores() - 1
 n_cores
 # 
 
 source("Source_Multiple_Runs.R")
 
 # the time difference between using n_cores and not using it
-shifting_curstart <- 1:4
+shifting_curstart <- 1:2
 paramsFile <- c("params.yaml")
 mclapply(shifting_curstart, multi_runs, mc.cores = n_cores)
 #mclapply(shifting_curstart, sum, mc.cores = n_cores)
