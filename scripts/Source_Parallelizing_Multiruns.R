ec01cd8b15ecd965285c58ee20e24b8876b385b2
prundstrom <parker.rundstrom@gmail.com>
2019-03-04 02:28:49 -0600

    cosmetic detailing

--- scripts/Source_Parallelizing_Multiruns.R
+++ scripts/Source_Parallelizing_Multiruns.R
@@ -1,15 +1,16 @@
+#setwd("/home/parker/Documents/projects/Code/curiosity-code/scripts")
 rm(list=objects())
 source("Source_Reference_Section.R")
 
 # we specify the number of cores/workers we want to use
-n_cores <- detectCores() - 1
+n_cores <- detectCores() - 4
 n_cores
 # 
 
 source("Source_Multiple_Runs.R")
 
 # the time difference between using n_cores and not using it
-shifting_curstart <- 1:2
+shifting_curstart <- 1:4
 paramsFile <- c("params.yaml")
 mclapply(shifting_curstart, multi_runs, mc.cores = n_cores)
 #mclapply(shifting_curstart, sum, mc.cores = n_cores)
