1ad7f5f2e5d6d10186beb116ac03b932ddf675ac
prundstrom <parker.rundstrom@gmail.com>
2019-03-06 10:51:48 -0600

    make more flexible for first-time remote repos

--- scripts/Source_Parallelizing_Multiruns.R
+++ scripts/Source_Parallelizing_Multiruns.R
@@ -1,7 +1,24 @@
 #setwd("/home/parker/Documents/projects/Code/curiosity-code/scripts")
 rm(list=objects())
 setwd(file.path(strsplit(getwd(), "curiosity-code")[[1]][1], "curiosity-code"))
+
+
+# Take care of first-time local repos that don't have the "extra folders" that are still vital - results/ and source/temp/
+if(!(dir.exists(file.path(strsplit(getwd(), 
+        "curiosity-code", )[[1]][1], "curiosity-code", "results")))) {
+          
+          dir.create(file.path(strsplit(getwd(), 
+        "curiosity-code", )[[1]][1], "curiosity-code", "results"))}
+
+if(!(dir.exists(file.path(strsplit(getwd(), 
+        "curiosity-code", )[[1]][1], "curiosity-code", "source", "temp")))) {
+          
+          dir.create(file.path(strsplit(getwd(), 
+        "curiosity-code", )[[1]][1], "curiosity-code", "source", "temp"))}
+
+# 
 source(file.path("scripts", "Source_Reference_Section.R"))
+
 
 # we specify the number of cores/workers we want to use
 n_cores <- detectCores() - 4
