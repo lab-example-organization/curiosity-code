f4beef0706970cb9f083ed0716d39998e191d881
rundstpj <prundstrom>
2019-05-30 16:20:07 -0500

    some basic stuff needed updoot

--- scripts/Source_Parallelizing_Multiruns.R
+++ scripts/Source_Parallelizing_Multiruns.R
@@ -57,7 +57,7 @@ source(file.path("scripts", "Source_Reference_Section.R"))
 referenceSection("multirun")
 # referenceSection("profiler")
 
-n_cores <- 16
+n_cores <- 4
 # Specify the number of cores/workers we want to use
     # n_cores <- detectCores() - 3 # built around a maximum allowance
 # n_cores <- 2
@@ -71,7 +71,7 @@ source(file.path("scripts", "Source_Multiple_Runs.R"))
 
 
 shifting_curstart <- 1:100
-paramsFile <- c("HghResLMHSmSxLB.yaml")
+paramsFile <- c("HghResLMHSmSxHB.yaml")
 # paramsFile <- c("diffZwischensTnN.yaml")
 simDate <- gsub('-', '', substring(Sys.Date(), 3))
 secretCode <- 58418
