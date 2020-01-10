bb6eeeaf4038095064a78c5e947ab5864a26bb25
prundstrom <parker.rundstrom@gmail.com>
2020-01-10 14:38:23 -0500

    set up files for daisychain redo

--- scripts/Source_Parallelizing_MultirunsNoInvP.R
+++ scripts/Source_Parallelizing_MultirunsNoInvP.R
@@ -71,7 +71,7 @@ source(file.path("scripts", "Source_Multiple_Runs.R"))
 
 
 shifting_curstart <- 1:200
-paramsfile <- c("paramsSmolInvMalHighHrTenK.yaml")
+paramsfile <- c("paramsparentNoInv.yaml")
 # paramsFile <- c("diffZwischensTnN.yaml")
 simdate <- gsub('-', '', substring(Sys.Date(), 3))
 secretcode <- 58418
