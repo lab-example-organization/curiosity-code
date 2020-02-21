9f3e5ea062a3e244b2d8da33a88390fc837a3182
prundstrom <parker.rundstrom@gmail.com>
2020-02-21 12:08:53 -0600

    propagating readLines fix

--- scripts/Source_Parallelizing_MultirunsLateSmolFemLowInvHRtenK.R
+++ scripts/Source_Parallelizing_MultirunsLateSmolFemLowInvHRtenK.R
@@ -71,7 +71,7 @@ source(file.path("scripts", "Source_Multiple_Runs.R"))
 
 
 shifting_curstart <- 1:200
-paramsfile <- c("paramsLateSnolInvFemLowHrTenK.yaml")
+paramsfile <- c("paramsLateSmolInvFemLowHrTenK.yaml")
 # paramsFile <- c("diffZwischensTnN.yaml")
 simdate <- gsub('-', '', substring(Sys.Date(), 3))
 secretcode <- 58418
