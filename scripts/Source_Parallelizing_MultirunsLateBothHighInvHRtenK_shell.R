23716ba0d0e1d231360285b2779622c1fc8e5b1f
prundstrom <parker.rundstrom@gmail.com>
2021-07-24 16:58:23 -0500

    next round core and shell

--- scripts/Source_Parallelizing_MultirunsLateBothHighInvHRtenK_shell.R
+++ scripts/Source_Parallelizing_MultirunsLateBothHighInvHRtenK_shell.R
@@ -70,8 +70,8 @@ n_cores <- 8
 source (file.path ("scripts", "Source_Multiple_Runs.R"))
 
 
-shifting_curstart <- 1 : 200
-paramsfile <- c ("paramsLateSmolInvBothHighHrTenK.yaml")
+shifting_curstart <- 1 : 168
+paramsfile <- c ("paramsLateSmolInvBothHighHrTenK_shell.yaml")
 # paramsFile <- c ("diffZwischensTnN.yaml")
 simdate <- gsub ('-', '', substring (Sys.Date(), 3))
 secretcode <- 58418
